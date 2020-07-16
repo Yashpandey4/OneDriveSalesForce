578b48fcb22bf0d544c501789262279f1b14e708
Yashpandey4 <yashpandey4@gmail.com>
2020-07-16 21:07:07 +0530

    Updated Node-Fetch dependency for HTTP Calls

--- uploadScript/script.js
+++ uploadScript/script.js
@@ -12,54 +12,67 @@ var onedrive_client_id = "insert-here";
 var onedrive_client_secret = "insert-here";
 var onedrive_refresh_token = "insert-here";
 
-function shareURL(token) {
-    request.post(
-        {
-            url: `https://graph.microsoft.com/v1.0/me/drive/items/root:/${onedrive_folder}/${onedrive_filename}:/createLink`,
+async function main() {
+    try {
+        var auth_response = await fetch("https://login.microsoftonline.com/common/oauth2/v2.0/token", {
+            method: "POST",
+            body: new URLSearchParams({
+                redirect_uri: "http://localhost/dashboard",
+                client_id: onedrive_client_id,
+                client_secret: onedrive_client_secret,
+                refresh_token: onedrive_refresh_token,
+                grant_type: "refresh_token",
+            }).toString(),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        var data = await response.json();
+        var token = data.access_token;
+        uploadFile(token);
+        shareURL(token);
+    }
+    catch(err) {
+        console.log(`Auth failed: ${err}`);
+    }
+    
+}
+
+async function uploadFile(token) {
+    fs.readFile(file, function read(e, f) {
+        try {
+            var upload_response = await fetch(`https://graph.microsoft.com/v1.0/me/drive/root:/${onedrive_folder}/${onedrive_filename}:/content`, {
+                method: "PUT",
+                body: f,
+                headers: {
+                    Authorization: `bearer ${token}`,
+                    "Content-Type": mime.getType(file),
+                }
+            })
+            console.log(`File uploaded successfully! ${upload_response.json()}`);
+        }
+        catch(er) {
+            console.log(`Upload failed: ${err}`);
+        }
+    });
+  }
+
+async function shareURL(token) {
+    try {
+        var share_response = await fetch(`https://graph.microsoft.com/v1.0/me/drive/items/root:/${onedrive_folder}/${onedrive_filename}:/createLink`, {
+            method: 'POST',
             headers: {
                 Authorization: `bearer ${token}`,
                 "Content-Type": mime.getType(file),
                 "type": "edit",
                 "scope": "anonymous"
-            },
-        },
-        function(er, re, bo) {
-            console.log(JSON.parse(bo).link.webUrl)
-        }
-    )
+            }
+        });
+        var data = await share_response.json();
+        console.log(`Shared URL of the File uploaded is ${data.link.webUrl}`);
+    }
+    catch(er) {
+        console.log(`Share failed: ${err}`);
+    }
 }
 
-function uploadFile(token) {
-  fs.readFile(file, function read(e, f) {
-    request.put(
-      {
-        url: `https://graph.microsoft.com/v1.0/me/drive/root:/${onedrive_folder}/${onedrive_filename}:/content`,
-        headers: {
-          Authorization: `bearer ${token}`,
-          "Content-Type": mime.getType(file),
-        },
-        body: f,
-      },
-      function (er, re, bo) {
-        console.log(bo);
-        shareURL(token);
-      }
-    );
-  });
-}
-
-request.post(
-  {
-    url: "https://login.microsoftonline.com/common/oauth2/v2.0/token",
-    form: {
-      redirect_uri: "http://localhost/dashboard",
-      client_id: onedrive_client_id,
-      client_secret: onedrive_client_secret,
-      refresh_token: onedrive_refresh_token,
-      grant_type: "refresh_token",
-    },
-  },
-  function (error, response, body) {
-    uploadFile(JSON.parse(body).access_token);
-  }
-);
